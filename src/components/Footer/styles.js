fe3d8c8986f86c97ad2f05a93575030d06c052c4
felipe <carlosfelipemonte1997@hotmail.com>
2023-10-28 10:28:08 -0300

    ajust footer

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -3,7 +3,8 @@ import styled from "styled-components";
 export const Container = styled.footer`
   width: 100%;
   height: 40px;
-  position: fixed;
+
+  position: absolute;
   bottom: 0;
   left: 0;
 
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -3,6 +3,9 @@ import styled from "styled-components";
 export const Container = styled.div`
   width: 100%;  
   height: 100%;
+  
+  padding-bottom: 40px;
+  position: relative;
 
   > main {
     width: 100%;
