b68d7609567440bfd1a3c4097400a932d6c8b501
Carlos Felipe <carlosfelipemonte1997@outlook.com>
2023-10-31 00:36:47 -0300

    teste code 2

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -2,6 +2,7 @@ import { FiArrowLeft } from 'react-icons/fi';
 import { Container, Section, ContainerImg, ContainerText, ButtonBackContainer } from './styles';
 import { useLocation } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom'; // Importe useNavigate
 import { Header } from '../../components/Header';
 import { Footer } from '../../components/Footer';
 
@@ -15,6 +16,8 @@ export function Details() {
     code: code || '',
     price: price || '',
   });
+
+  const navigate = useNavigate(); // Use useNavigate para navegar
 
   useEffect(() => {
     // Salve os dados no localStorage quando houver alterações
@@ -30,6 +33,19 @@ export function Details() {
       setProductData({ imgUrl, title, code, price });
     }
   }, []);
+
+  useEffect(() => {
+    const handleBeforeUnload = (event) => {
+      // Navegue para a página de detalhes novamente quando a página for recarregada
+      navigate(`/details/${code}`);
+    };
+
+    window.addEventListener('beforeunload', handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload);
+    };
+  }, [code, navigate]);
 
   return (
     <Container>
