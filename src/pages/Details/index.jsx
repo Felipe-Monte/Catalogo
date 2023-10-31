0030b52e6494200edc81db418c9b8c189c95eb70
Carlos Felipe <carlosfelipemonte1997@outlook.com>
2023-10-31 00:40:22 -0300

    test bug

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,14 +1,34 @@
-import { FiArrowLeft } from 'react-icons/fi'
-import { Container, Section, ContainerImg, ContainerText,ButtonBackContainer } from './styles'
-import { useLocation } from 'react-router-dom'
-
-import { Header } from '../../components/Header'
-import { Footer } from '../../components/Footer'
+import { FiArrowLeft } from 'react-icons/fi';
+import { Container, Section, ContainerImg, ContainerText, ButtonBackContainer } from './styles';
+import { useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom'; // Importe useNavigate
+import { Header } from '../../components/Header';
+import { Footer } from '../../components/Footer';
 
 export function Details() {
-  const location = useLocation()
-  const { imgUrl, title, code, price } = location.state || {}
+  const location = useLocation();
+  const { imgUrl, title, code, price } = location.state || {};
 
+  const navigate = useNavigate(); // Use useNavigate para navegar
+
+  useEffect(() => {
+    // Salve os dados no localStorage
+    localStorage.setItem('productDetails', JSON.stringify({ imgUrl, title, code, price }));
+  }, [imgUrl, title, code, price]);
+
+  useEffect(() => {
+    const handleBeforeUnload = (event) => {
+      // Navegue de volta para a página inicial (Home) quando a página for recarregada
+      navigate(-1);
+    };
+
+    window.addEventListener('beforeunload', handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload);
+    };
+  }, [navigate]);
 
   return (
     <Container>
@@ -36,7 +56,6 @@ export function Details() {
       </main>
 
       {/* <Footer /> */}
-
     </Container>
-  )
+  );
 }
