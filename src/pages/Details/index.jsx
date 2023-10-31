b757289c2ed24333a00e59aa03aa94335633db6d
Carlos Felipe <carlosfelipemonte1997@outlook.com>
2023-10-30 23:56:43 -0300

    test local storage

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -2,6 +2,8 @@ import { FiArrowLeft } from 'react-icons/fi'
 import { Container, Section, ContainerImg, ContainerText,ButtonBackContainer } from './styles'
 import { useLocation } from 'react-router-dom'
 
+import { useEffect } from 'react'
+
 import { Header } from '../../components/Header'
 import { Footer } from '../../components/Footer'
 
@@ -9,6 +11,19 @@ export function Details() {
   const location = useLocation()
   const { imgUrl, title, code, price } = location.state || {}
 
+  useEffect(() => {
+    // Salve os dados no localStorage
+    localStorage.setItem('productDetails', JSON.stringify({ imgUrl, title, code, price }));
+  }, [imgUrl, title, code, price]);
+  
+  useEffect(() => {
+    // Recupere os dados do localStorage
+    const storedData = localStorage.getItem('productDetails');
+    if (storedData) {
+      const { imgUrl, title, code, price } = JSON.parse(storedData);
+      // Atualize o estado local com os dados recuperados
+    }
+  }, []);
 
   return (
     <Container>
