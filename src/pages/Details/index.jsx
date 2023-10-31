8d5e561d2e79906b7b0f70b41d2c771a287a4a31
Carlos Felipe <carlosfelipemonte1997@outlook.com>
2023-10-31 00:38:23 -0300

    back code

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,51 +1,14 @@
-import { FiArrowLeft } from 'react-icons/fi';
-import { Container, Section, ContainerImg, ContainerText, ButtonBackContainer } from './styles';
-import { useLocation } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom'; // Importe useNavigate
-import { Header } from '../../components/Header';
-import { Footer } from '../../components/Footer';
+import { FiArrowLeft } from 'react-icons/fi'
+import { Container, Section, ContainerImg, ContainerText,ButtonBackContainer } from './styles'
+import { useLocation } from 'react-router-dom'
+
+import { Header } from '../../components/Header'
+import { Footer } from '../../components/Footer'
 
 export function Details() {
-  const location = useLocation();
-  const { imgUrl, title, code, price } = location.state || {};
+  const location = useLocation()
+  const { imgUrl, title, code, price } = location.state || {}
 
-  const [productData, setProductData] = useState({
-    imgUrl: imgUrl || '',
-    title: title || '',
-    code: code || '',
-    price: price || '',
-  });
-
-  const navigate = useNavigate(); // Use useNavigate para navegar
-
-  useEffect(() => {
-    // Salve os dados no localStorage quando houver alterações
-    localStorage.setItem('productDetails', JSON.stringify(productData));
-  }, [productData]);
-
-  useEffect(() => {
-    // Recupere os dados do localStorage quando a página for montada
-    const storedData = localStorage.getItem('productDetails');
-    if (storedData) {
-      const { imgUrl, title, code, price } = JSON.parse(storedData);
-      // Atualize o estado local com os dados recuperados
-      setProductData({ imgUrl, title, code, price });
-    }
-  }, []);
-
-  useEffect(() => {
-    const handleBeforeUnload = (event) => {
-      // Navegue para a página de detalhes novamente quando a página for recarregada
-      navigate(`/details/${code}`);
-    };
-
-    window.addEventListener('beforeunload', handleBeforeUnload);
-
-    return () => {
-      window.removeEventListener('beforeunload', handleBeforeUnload);
-    };
-  }, [code, navigate]);
 
   return (
     <Container>
@@ -55,17 +18,17 @@ export function Details() {
         <div className='container-section'>
           <Section>
             <ContainerImg>
-              <img src={productData.imgUrl} alt={productData.title} />
+              <img src={imgUrl} />
             </ContainerImg>
 
             <ContainerText>
-              <h2>{productData.title}</h2>
-              <p>{productData.code}</p>
-              <span>R$: {productData.price}</span>
+              <h2>{title}</h2>
+              <p>{code}</p>
+              <span>R$: {price}</span>
             </ContainerText>
 
             <ButtonBackContainer>
-              <FiArrowLeft />
+              <FiArrowLeft/>
               <a href="#">voltar</a>
             </ButtonBackContainer>
           </Section>
@@ -73,6 +36,7 @@ export function Details() {
       </main>
 
       {/* <Footer /> */}
+
     </Container>
-  );
+  )
 }
