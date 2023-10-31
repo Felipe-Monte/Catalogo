9382d733405056d0009607d4161929b41fb24378
Carlos Felipe <carlosfelipemonte1997@outlook.com>
2023-10-31 00:04:37 -0300

    back code

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,36 +1,14 @@
-import { FiArrowLeft } from 'react-icons/fi';
-import { Container, Section, ContainerImg, ContainerText, ButtonBackContainer } from './styles';
-import { useLocation } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import { Header } from '../../components/Header';
-import { Footer } from '../../components/Footer';
+import { FiArrowLeft } from 'react-icons/fi'
+import { Container, Section, ContainerImg, ContainerText,ButtonBackContainer } from './styles'
+import { useLocation } from 'react-router-dom'
+
+import { Header } from '../../components/Header'
+import { Footer } from '../../components/Footer'
 
 export function Details() {
-  const location = useLocation();
-  const { imgUrl, title, code, price } = location.state || {};
+  const location = useLocation()
+  const { imgUrl, title, code, price } = location.state || {}
 
-  const [productData, setProductData] = useState({
-    imgUrl: imgUrl || '',
-    title: title || '',
-    code: code || '',
-    price: price || '',
-  });
-
-  // Função para atualizar o estado e o localStorage
-  const updateProductData = (data) => {
-    setProductData(data);
-    localStorage.setItem('productDetails', JSON.stringify(data));
-  };
-
-  useEffect(() => {
-    // Recupere os dados do localStorage ao montar o componente
-    const storedData = localStorage.getItem('productDetails');
-    if (storedData) {
-      const { imgUrl, title, code, price } = JSON.parse(storedData);
-      // Atualize o estado local com os dados recuperados
-      updateProductData({ imgUrl, title, code, price });
-    }
-  }, []);
 
   return (
     <Container>
@@ -40,17 +18,17 @@ export function Details() {
         <div className='container-section'>
           <Section>
             <ContainerImg>
-              <img src={productData.imgUrl} alt={productData.title} />
+              <img src={imgUrl} />
             </ContainerImg>
 
             <ContainerText>
-              <h2>{productData.title}</h2>
-              <p>{productData.code}</p>
-              <span>R$: {productData.price}</span>
+              <h2>{title}</h2>
+              <p>{code}</p>
+              <span>R$: {price}</span>
             </ContainerText>
 
             <ButtonBackContainer>
-              <FiArrowLeft />
+              <FiArrowLeft/>
               <a href="#">voltar</a>
             </ButtonBackContainer>
           </Section>
@@ -58,6 +36,7 @@ export function Details() {
       </main>
 
       {/* <Footer /> */}
+
     </Container>
-  );
+  )
 }
