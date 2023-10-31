a1addc2fa7d05032617b2d00d252d487c86b7805
Carlos Felipe <carlosfelipemonte1997@outlook.com>
2023-10-30 23:54:35 -0300

    back commit

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,7 +1,7 @@
 import { FiArrowLeft } from 'react-icons/fi'
-import { Container, Section, ContainerImg, ContainerText, ButtonBackContainer } from './styles'
+import { Container, Section, ContainerImg, ContainerText,ButtonBackContainer } from './styles'
 import { useLocation } from 'react-router-dom'
-import { useEffect, useState } from 'react'
+
 import { Header } from '../../components/Header'
 import { Footer } from '../../components/Footer'
 
@@ -9,18 +9,6 @@ export function Details() {
   const location = useLocation()
   const { imgUrl, title, code, price } = location.state || {}
 
-  const [productData, setProductData] = useState({ imgUrl, title, code, price});
-
-  useEffect(() => {
-    // Recupere os dados do localStorage
-    const storedProductDetails = localStorage.getItem('productDetails');
-
-    if (storedProductDetails) {
-      const { imgUrl, title, code, price } = JSON.parse(storedProductDetails);
-      // Atualize o estado local com os dados recuperados
-      setProductData({ imgUrl, title, code, price });
-    }
-  }, []);
 
   return (
     <Container>
@@ -30,17 +18,17 @@ export function Details() {
         <div className='container-section'>
           <Section>
             <ContainerImg>
-              <img src={productData.imgUrl} alt={productData.title} />
+              <img src={imgUrl} />
             </ContainerImg>
 
             <ContainerText>
-              <h2>{productData.title}</h2>
-              <p>{productData.code}</p>
-              <span>R$: {productData.price}</span>
+              <h2>{title}</h2>
+              <p>{code}</p>
+              <span>R$: {price}</span>
             </ContainerText>
 
             <ButtonBackContainer>
-              <FiArrowLeft />
+              <FiArrowLeft/>
               <a href="#">voltar</a>
             </ButtonBackContainer>
           </Section>
@@ -48,6 +36,7 @@ export function Details() {
       </main>
 
       {/* <Footer /> */}
+
     </Container>
   )
 }
