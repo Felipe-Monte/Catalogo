ad6b7dd121bd7a7c8d407a6b6acaea83ed49d946
felipe <carlosfelipemonte1997@hotmail.com>
2023-11-07 15:23:03 -0300

    add cors and axios

--- src/pages/AddCard/index.jsx
+++ src/pages/AddCard/index.jsx
@@ -1,32 +1,56 @@
 import { Container } from './styles'
 
 import { Header } from '../../components/Header'
+import { useState } from 'react'
 
 export function AddCard() {
+  const [title, setTitle] = useState("")
+  const [code, setCode] = useState("")
+  const [price, setPrice] = useState("")
+
+  async function handleNewCard(){
+
+  }
+
   return (
     <Container>
       <Header title="Adicionar novo produto" />
-      
-      <form action="/cards" method='post' >
+
+      <form>
 
         <div className='container_input'>
           <label htmlFor="image">Imagem:</label>
-          <input type="file" id='image' accept='image/*'/> 
+          <input
+            type="file"
+            id='image'
+          />
         </div>
 
         <div className='container_input'>
           <label htmlFor="title">Título:</label>
-          <input type="text" id='title' name='title' placeholder='Descrição do produto'/>
+          <input
+            type="text"
+            id='title'
+            placeholder='Descrição do produto'
+            onChange={e => setTitle(e.target.value)} />
         </div>
 
         <div className='container_input'>
           <label htmlFor="code">Código:</label>
-          <input type="text" id='code' name='code' placeholder='Cód: 00000'/>
+          <input
+            type="text"
+            id='code'
+            placeholder='Cód: 00000'
+            onChange={e => setCode(e.target.value)} />
         </div>
 
         <div className='container_input'>
           <label htmlFor="price">Preço:</label>
-          <input type="text" id='price' name='price' placeholder='R$: 00,00'/>
+          <input
+            type="text"
+            id='price'
+            placeholder='R$: 00,00' 
+            onChange={e => setPrice(e.target.value)}/>
         </div>
 
         <button>Enviar</button>
