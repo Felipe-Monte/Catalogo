4a0446f5d3ab9fde0beeca4cf83eafb805f6db02
felipe <carlosfelipemonte1997@hotmail.com>
2023-11-09 13:40:57 -0300

    conect api product

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,11 +2,30 @@ import { Container, Section } from "./styles"
 
 import { Header } from "../../components/Header"
 import { Cards } from "../../components/Cards"
-import { Footer } from "../../components/Footer"
 
 import jsonData from "../../products.json"
+import { api } from "../../services/api"
+import { useState, useEffect } from "react"
 
 export function Home() {
+  const [cards, setCards] = useState([])
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await api.get("/cards")
+        setCards(response.data)
+
+        console.log(response.data)
+
+      } catch (error) {
+        console.log("Error na requisição:", error)
+      }
+    }
+
+    fetchData()
+  }, [])
+
   return (
     <Container>
       <Header
@@ -16,13 +35,13 @@ export function Home() {
       <main>
         <Section>
           {
-            jsonData.map((item) => (
+            jsonData.map((card) => (
               <Cards
-                key={item.id}
-                imgUrl={item.imgUrl}
-                title={item.title}
-                code={item.code}
-                price={item.price}
+                key={card.id}
+                imgUrl={card.imgUrl}
+                title={card.title}
+                code={card.code}
+                price={card.price}
               />
             ))
           }
