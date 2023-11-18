7fb8f4b2cbbc0a1bb9d9f278cc3515b7a852f427
felipe <carlosfelipemonte1997@hotmail.com>
2023-11-18 10:45:55 -0300

    add search code

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,31 +1,35 @@
-import { Container, Section } from "./styles";
-import { FiSearch } from 'react-icons/fi';
-import { Header } from "../../components/Header";
-import { Cards } from "../../components/Cards";
-import { api } from "../../services/api";
-import { useState, useEffect } from "react";
-import { Input } from '../../components/Input';
+import { Container, Section } from "./styles"
+import { FiSearch } from 'react-icons/fi'
+
+import { useState, useEffect } from "react"
+
+import { Header } from "../../components/Header"
+import { Cards } from "../../components/Cards"
+import { Input } from '../../components/Input'
+
+import { api } from "../../services/api"
 
 export function Home() {
-  const [cards, setCards] = useState([]);
-  const [search, setSearch] = useState('');
+  const [cards, setCards] = useState([])
+  const [search, setSearch] = useState('')
 
   useEffect(() => {
     async function fetchData() {
       try {
-        const response = await api.get("/cards");
-        setCards(response.data);
+        const response = await api.get("/cards")
+        setCards(response.data)
       } catch (error) {
-        console.log("Error na requisição:", error);
+        console.log("Error na requisição:", error)
       }
     }
 
-    fetchData();
-  }, []);
+    fetchData()
+  }, [])
 
   const filteredCards = cards.filter(card =>
-    card.title.toLowerCase().includes(search.toLowerCase())
-  );
+    card.title.toLowerCase().includes(search.toLowerCase()) ||
+    card.code.toLowerCase().includes(search.toLowerCase())
+  )
 
   return (
     <Container>
@@ -35,7 +39,7 @@ export function Home() {
 
       <Input
         icon={FiSearch}
-        placeholder="Pesquisar por nome"
+        placeholder="Pesquisar por nome ou código"
         onChange={(e) => setSearch(e.target.value)}
       />
 
