72a24222db8a0c9ae9e254ed68e098a78cd8dd75
felipe <carlosfelipemonte1997@hotmail.com>
2023-11-18 10:36:21 -0300

    add filter in api cards

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,45 +1,31 @@
-import { Container, Section } from "./styles"
-import { FiSearch } from 'react-icons/fi'
-
-import { Header } from "../../components/Header"
-import { Cards } from "../../components/Cards"
-
-import jsonData from "../../products.json"
-import { api } from "../../services/api"
-import { useState, useEffect } from "react"
-
-import { Input } from '../../components/Input'
+import { Container, Section } from "./styles";
+import { FiSearch } from 'react-icons/fi';
+import { Header } from "../../components/Header";
+import { Cards } from "../../components/Cards";
+import { api } from "../../services/api";
+import { useState, useEffect } from "react";
+import { Input } from '../../components/Input';
 
 export function Home() {
-  // const [cards, setCards] = useState([])
-
-  const [search, setSearch] = useState("")
-  const [filteredData, setFilteredData] = useState(jsonData)
-
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     try {
-  //       const response = await api.get("/cards")
-  //       setCards(response.data)
-
-  //     } catch (error) {
-  //       console.log("Error na requisição:", error)
-  //     }
-  //   }
-
-  //   fetchData()
-  // }, [])
+  const [cards, setCards] = useState([]);
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
-    const filteredData = () => {
-      const filtered = jsonData.filter(card => 
-        card.title.toLowerCase().includes(search.toLowerCase())
-      )
-      setFilteredData(filtered)
+    async function fetchData() {
+      try {
+        const response = await api.get("/cards");
+        setCards(response.data);
+      } catch (error) {
+        console.log("Error na requisição:", error);
       }
-   
- filteredData()
-  }, [search])
+    }
+
+    fetchData();
+  }, []);
+
+  const filteredCards = cards.filter(card =>
+    card.title.toLowerCase().includes(search.toLowerCase())
+  );
 
   return (
     <Container>
@@ -50,31 +36,24 @@ export function Home() {
       <Input
         icon={FiSearch}
         placeholder="Pesquisar por nome"
-        value={search}
         onChange={(e) => setSearch(e.target.value)}
       />
 
       <main>
         <Section>
-          {
-            filteredData.map((card) => (
-              <Cards
-                key={card.id}
-                imgUrl={card.imgUrl}
-                title={card.title}
-                code={card.code}
-                price={`R$: ${card.price}`}
-              />
-            ))
-          }
+          {filteredCards.map((card) => (
+            <Cards
+              key={card.id}
+              imgUrl={`${api.defaults.baseURL}/files/${card.imgUrl}`}
+              title={card.title}
+              code={card.code}
+              price={`R$: ${card.price}`}
+            />
+          ))}
         </Section>
       </main>
 
-      {/* `${api.defaults.baseURL}/files/${card.imgUrl}` */}
-
       {/* <Footer /> */}
     </Container>
-  )
+  );
 }
-
-
