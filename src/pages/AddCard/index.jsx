a18e8ea2b23c5a4d03cbd6f94bc93a14af03adc4
felipe <carlosfelipemonte1997@hotmail.com>
2023-11-24 14:54:43 -0300

    cleaner inputs

--- src/pages/AddCard/index.jsx
+++ src/pages/AddCard/index.jsx
@@ -1,54 +1,64 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
-import { Container } from './styles';
-import { FiArrowLeft } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
-import { Header } from '../../components/Header';
-import { Input } from '../../components/Input';
+import { Container } from './styles'
+import { FiArrowLeft } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
+import { Header } from '../../components/Header'
+import { Input } from '../../components/Input'
 
-import { api } from '../../services/api';
+import { api } from '../../services/api'
 
 export function AddCard() {
-  const [avatar, setAvatar] = useState(null);
-  const [avatarFile, setAvatarFile] = useState(null);
+  const [avatar, setAvatar] = useState(null)
+  const [avatarFile, setAvatarFile] = useState(null)
+  const [title, setTitle] = useState('')
+  const [code, setCode] = useState('')
+  const [price, setPrice] = useState('')
 
-  const [title, setTitle] = useState('');
-  const [code, setCode] = useState('');
-  const [price, setPrice] = useState('');
+  const [isSubmitting, setIsSubmitting] = useState(false)
 
   const handleAddImg = (event) => {
-    const file = event.target.files[0];
-    setAvatarFile(file);
-    setAvatar(URL.createObjectURL(file));
-  };
+    const file = event.target.files[0]
+    setAvatarFile(file)
+    setAvatar(URL.createObjectURL(file))
+  }
 
   const handleNewCard = async (e) => {
-    e.preventDefault();
+    e.preventDefault()
 
-    if (!avatarFile || !title || !code || !price) {
-      return alert('Preencha todos os campos');
+    if (!avatarFile || !title || !code || !price || isSubmitting) {
+      return alert('Preencha todos os campos e aguarde o cadastro terminar')
     }
 
-    const formData = new FormData();
-    formData.append('upload', avatarFile);
-    formData.append('title', title);
-    formData.append('code', code);
-    formData.append('price', price);
+    setIsSubmitting(true)
 
+    const formData = new FormData()
+    formData.append('upload', avatarFile)
+    formData.append('title', title)
+    formData.append('code', code)
+    formData.append('price', price)
 
     try {
       const response = await api.post("/cards/upload", formData, {
         headers: { 'Content-Type': 'multipart/form-data' },
-      });
+      })
 
-      console.log(response.data);
-      alert('Produto cadastrado com sucesso!');
+      console.log(response.data)
+      alert('Produto cadastrado com sucesso!')
 
-    } catch (error) { 
-      console.error('Erro ao enviar o formulário:', error);
-      alert('Erro ao enviar');
+      setAvatar(null)
+      setAvatarFile(null)
+      setTitle('')
+      setCode('')
+      setPrice('')
+
+    } catch (error) {
+      console.error('Erro ao enviar o formulário:', error)
+      alert('Erro ao enviar')
+    } finally {
+      setIsSubmitting(false)
     }
-  };
+  }
 
   return (
     <Container>
@@ -76,16 +86,19 @@ export function AddCard() {
         <div className="container_input_text">
           <Input
             placeholder="Título"
+            value={title}
             onChange={(e) => setTitle(e.target.value)}
           />
 
           <Input
             placeholder="Código do produto"
+            value={code}
             onChange={(e) => setCode(e.target.value)}
           />
 
           <Input
             placeholder="Preço"
+            value={price}
             onChange={(e) => setPrice(e.target.value)}
           />
         </div>
@@ -99,13 +112,14 @@ export function AddCard() {
           <button
             type="submit"
             onClick={handleNewCard}
+            disabled={isSubmitting}
           >
-            Enviar
+            {isSubmitting ? 'Enviando...' : 'Enviar'}
           </button>
         </div>
 
       </form>
 
     </Container>
-  );
+  )
 }
