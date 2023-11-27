057b15161d4f5c0a8e9da3e3abd1f30499b25c60
Carlos Felipe <carlosfelipemonte1997@outlook.com>
2023-11-27 19:12:05 -0300

    remove sidebar

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,21 +1,13 @@
-import { useState } from "react"
 import { Container } from "./styles"
 
 import { FiAlignJustify } from 'react-icons/fi'
 
-import { Sidebar } from "../Sidebar"
-
 export function Header({ title }) {
-  const [sidebar, setSidebar] = useState(false)
-
-  const showSidebar = () => setSidebar(!sidebar)
-
   return (
     <Container>
       <h1>{title}</h1>
 
-      <FiAlignJustify onClick={showSidebar} />
-      {sidebar && <Sidebar active={setSidebar} />}
+      <FiAlignJustify/>
     </Container>
   )
 }
