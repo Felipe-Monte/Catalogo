ce3c21c115154c7bb03708279851f1989e6c7339
Carlos Felipe <carlosfelipemonte1997@outlook.com>
2023-11-27 21:18:17 -0300

    test img

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,20 +1,38 @@
-import { FiArrowLeft } from 'react-icons/fi'
-import { FaWhatsapp } from "react-icons/fa";
-import { Container, Section, WrapperContainer, ContainerImg, ContainerText, ContainerButton } from './styles'
-import { useLocation } from 'react-router-dom'
-
-import { Header } from '../../components/Header'
-
-import { useNavigate } from 'react-router-dom'
+import React from 'react';
+import { FiArrowLeft } from 'react-icons/fi';
+import { FaWhatsapp } from 'react-icons/fa';
+import { Container, Section, WrapperContainer, ContainerImg, ContainerText, ContainerButton } from './styles';
+import { useLocation } from 'react-router-dom';
+import { Header } from '../../components/Header';
+import { useNavigate } from 'react-router-dom';
 
 export function Details() {
-  const location = useLocation()
-  const { imgUrl, title, code, price } = location.state || {}
-
-  const navigate = useNavigate()
+  const location = useLocation();
+  const { imgUrl, title, code, price } = location.state || {};
+  const navigate = useNavigate();
 
   function handleClickBack() {
-    navigate("/")
+    navigate('/');
+  }
+
+  function handleShareOnWhatsApp() {
+    const message = `Confira este produto: ${title} - ${price}`;
+    const encodedMessage = encodeURIComponent(message);
+    const encodedUrl = encodeURIComponent(imgUrl);
+
+    if (navigator.share) {
+      // Se o navegador suportar a API de compartilhamento, use-a
+      navigator.share({
+        title: 'Detalhes do Produto',
+        text: message,
+        url: imgUrl,
+      })
+      .catch((error) => console.error('Error sharing:', error));
+    } else {
+      // Caso contrário, abrir uma nova janela com a URL do WhatsApp
+      const url = `whatsapp://send?text=${encodedMessage}%20${encodedUrl}`;
+      window.open(url, '_blank');
+    }
   }
 
   return (
@@ -24,7 +42,7 @@ export function Details() {
       <main>
         <Section>
           <ContainerImg>
-            <img src={imgUrl} />
+            <img src={imgUrl} alt={title} />
           </ContainerImg>
 
           <WrapperContainer>
@@ -33,30 +51,83 @@ export function Details() {
               <p>Cód: {code}</p>
               <span>{price}</span>
 
-              <FaWhatsapp />
-
+              {/* Adicionando o ícone do WhatsApp com a função de compartilhamento */}
+              <FaWhatsapp onClick={handleShareOnWhatsApp} style={{ cursor: 'pointer' }} />
             </ContainerText>
 
             <ContainerButton>
-              <button
-                id='btn_back'
-                onClick={handleClickBack}
-              >
+              <button id="btn_back" onClick={handleClickBack}>
                 <FiArrowLeft />
                 Voltar
               </button>
-              {/* <button
-                id='btn_delete'
-                onClick={handleClickDelete}
-              >
-                Deletar
-              </button> */}
             </ContainerButton>
           </WrapperContainer>
-
         </Section>
       </main>
-
     </Container>
-  )
+  );
 }
+
+
+// import { FiArrowLeft } from 'react-icons/fi'
+// import { FaWhatsapp } from "react-icons/fa";
+// import { Container, Section, WrapperContainer, ContainerImg, ContainerText, ContainerButton } from './styles'
+// import { useLocation } from 'react-router-dom'
+
+// import { Header } from '../../components/Header'
+
+// import { useNavigate } from 'react-router-dom'
+
+// export function Details() {
+//   const location = useLocation()
+//   const { imgUrl, title, code, price } = location.state || {}
+
+//   const navigate = useNavigate()
+
+//   function handleClickBack() {
+//     navigate("/")
+//   }
+
+//   return (
+//     <Container>
+//       <Header title="Detalhes do produto" />
+
+//       <main>
+//         <Section>
+//           <ContainerImg>
+//             <img src={imgUrl} />
+//           </ContainerImg>
+
+//           <WrapperContainer>
+//             <ContainerText>
+//               <h2>{title}</h2>
+//               <p>Cód: {code}</p>
+//               <span>{price}</span>
+
+//               <FaWhatsapp />
+
+//             </ContainerText>
+
+//             <ContainerButton>
+//               <button
+//                 id='btn_back'
+//                 onClick={handleClickBack}
+//               >
+//                 <FiArrowLeft />
+//                 Voltar
+//               </button>
+//               {/* <button
+//                 id='btn_delete'
+//                 onClick={handleClickDelete}
+//               >
+//                 Deletar
+//               </button> */}
+//             </ContainerButton>
+//           </WrapperContainer>
+
+//         </Section>
+//       </main>
+
+//     </Container>
+//   )
+// }
