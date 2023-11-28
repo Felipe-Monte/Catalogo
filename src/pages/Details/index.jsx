ed553495fc7c558b4dbb3904756a7efb4976130f
Carlos Felipe <carlosfelipemonte1997@outlook.com>
2023-11-27 21:35:45 -0300

    img test

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
-import { FaWhatsapp } from "react-icons/fa";
+import { FaWhatsapp } from 'react-icons/fa';
 import { Container, Section, WrapperContainer, ContainerImg, ContainerText, ContainerButton } from './styles';
 import { Header } from '../../components/Header';
 
@@ -16,7 +16,7 @@ export function Details() {
 
   const enviarWhatsApp = () => {
     const mensagem = `Confira esta imagem do produto ${title} (Cód: ${code}, Preço: ${price}).`;
-    const whatsappURL = `https://wa.me/?text=${encodeURIComponent(mensagem)}&attachment=${encodeURIComponent(imgUrl)}`;
+    const whatsappURL = `https://api.whatsapp.com/send?text=${encodeURIComponent(mensagem)}&amp;image=${encodeURIComponent(imgUrl)}`;
     window.open(whatsappURL);
   };
 
