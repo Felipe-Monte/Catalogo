9b4ce7ab9b3c27304806ede4117a633f1c9bd27b
felipe <carlosfelipemonte1997@hotmail.com>
2023-11-30 08:13:10 -0300

    ajust navigate

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -9,12 +9,10 @@ export function Details() {
   const location = useLocation();
   const {share, imgUrl, title, code, price } = location.state || {};
   
-  console.log(share)
-
   const navigate = useNavigate();
 
   const handleClickBack = () => {
-    navigate('/');
+    navigate(-1);
   };
 
   const enviarWhatsApp = () => {
@@ -55,67 +53,3 @@ export function Details() {
   );
 }
 
-
-
-// import { FiArrowLeft } from 'react-icons/fi'
-// import { FaWhatsapp } from "react-icons/fa";
-// import { Container, Section, WrapperContainer, ContainerImg, ContainerText, ContainerButton } from './styles'
-// import { useLocation } from 'react-router-dom'
-
-// import { Header } from '../../components/Header'
-
-// import { useNavigate } from 'react-router-dom'
-
-// export function Details() {
-//   const location = useLocation()
-//   const { imgUrl, title, code, price } = location.state || {}
-
-//   const navigate = useNavigate()
-
-//   function handleClickBack() {
-//     navigate("/")
-//   }
-
-//   return (
-//     <Container>
-//       <Header title="Detalhes do produto" />
-
-//       <main>
-//         <Section>
-//           <ContainerImg>
-//             <img src={imgUrl} />
-//           </ContainerImg>
-
-//           <WrapperContainer>
-//             <ContainerText>
-//               <h2>{title}</h2>
-//               <p>Cód: {code}</p>
-//               <span>{price}</span>
-
-//               <FaWhatsapp />
-
-//             </ContainerText>
-
-//             <ContainerButton>
-//               <button
-//                 id='btn_back'
-//                 onClick={handleClickBack}
-//               >
-//                 <FiArrowLeft />
-//                 Voltar
-//               </button>
-//               {/* <button
-//                 id='btn_delete'
-//                 onClick={handleClickDelete}
-//               >
-//                 Deletar
-//               </button> */}
-//             </ContainerButton>
-//           </WrapperContainer>
-
-//         </Section>
-//       </main>
-
-//     </Container>
-//   )
-// }
