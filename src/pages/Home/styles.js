16e48b6da669d9548654afa192a123a89d8f0aab
felipe <carlosfelipemonte1997@hotmail.com>
2023-12-05 09:59:10 -0300

    ajust css

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,33 +1,26 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  width: 100%;  
-  height: 100vh;
-
+  width: 100%;
+  min-height: 100vh;
   margin-top: 50px;
-  
   padding-bottom: 40px;
   position: relative;
-
   scroll-behavior: smooth;
 
   > main {
     width: 100%;
     max-width: 1500px;
-    height: 100vh;
-
-    margin: 0px auto;
+    min-height: 100vh;
+    margin: 0 auto;
     overflow-y: auto;
-
     display: flex;
     justify-content: center;
     align-items: center;
     align-content: flex-start;
     flex-wrap: wrap;
     gap: 15px;
-
-    padding: 0px;
-
+    padding: 0;
     scroll-behavior: smooth;
   }
 
@@ -38,36 +31,31 @@ export const Container = styled.div`
   @media (max-width: 1300px) {
     margin: 50px auto;
   }
-`
+`;
 
 export const Section = styled.section`
   width: 100%;
-
   display: flex;
   justify-content: center;
   flex-wrap: wrap;
   gap: 15px;
-
   padding: 15px;
-
   scroll-behavior: smooth;
 
-  /* border: 2px solid white; */
-
   @media screen and (max-width: 700px) {
-      display: grid;
-      grid-template-columns: repeat(2, 1fr);  
-      justify-content: center;
-      align-items: center;
-      justify-items: center;
-      gap: 3px;
-      
-      padding: 5px 0px 0px 0px;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);  
+    justify-content: center;
+    align-items: center;
+    justify-items: center;
+    gap: 3px;
+    padding: 5px 0 0 0;
 
-      > svg {
-        position: absolute;
-        top: 50px;
-      }
+    > svg {
+      position: absolute;
+      top: 50px;
     }
-`
+  }
+`;
+
 
