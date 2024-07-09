7028787da158bb0a6862ae268c71febfce2c5d07
felipe <carlosfelipemonte1997@hotmail.com>
2024-07-09 09:26:16 -0300

    feat: remove date in footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,15 +1,11 @@
-import { useState } from "react";
 import { Container } from "./styles";
 
 export function Footer({ title }) {
-  const [date, setdate] = useState(new Date().toLocaleDateString())
-  const [hour, setHour] = useState(new Date().toLocaleTimeString())
-
+  
   return (
     <Container>
       <p>
-        <i>Felipe</i> -
-        Atualizado dia {date} as {hour}
+        Desenvolvido por <i>Felipe</i>
       </p>
     </Container>
   );
