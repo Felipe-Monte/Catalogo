ec7d54b9251a31b2c7c6ff570682a51f3aba6a1f
Felipe <carlosfelipemonte1997@gmail.com>
2025-01-30 09:25:23 -0300

    Feat: add pagination

--- src/pages/List/index.jsx
+++ src/pages/List/index.jsx
@@ -11,73 +11,73 @@ const List = () => {
       alert('Por favor, insira os códigos dos produtos.');
       return;
     }
-
-    // Juntando todos o caracteres do list antes de verificar se tem numeros
+  
     const cleanedList = list.replace(/\s+/g, '');
-
     if (!/^\d+$/.test(cleanedList)) {
       alert('Por favor, insira apenas números.');
       return;
     }
-
+  
     const codes = list
       .split('\n')
       .map((code) => code.trim())
       .filter((code) => code !== '');
-
+  
     const doc = new jsPDF();
-
-    // Contar a quantidade de itens
+    const pageHeight = doc.internal.pageSize.height; // Altura da página em pontos
+    const marginTop = 20;
+    const lineSpacing = 10; // Espaço entre cada linha de texto
+    let yPosition = marginTop + 10; // Posição inicial do texto na página
+  
+    // Contagem de produtos encontrados e não encontrados
     const totalItems = codes.length;
-
-    // Contar a quantidade de itens não encontrados
     const notFoundCount = codes.filter((code) => {
-      const foundProduct = jsonData.find(
-        (item) => item.code.toLowerCase() === code.toLowerCase(),
-      );
-      return !foundProduct;
+      return !jsonData.find((item) => item.code.toLowerCase() === code.toLowerCase());
     }).length;
-
-    // Definir fonte 'helvetica'
+  
+    // Cabeçalho do PDF
     doc.setFont('helvetica', 'normal');
-    doc.setFontSize(12);
-
-    // Título do PDF com a quantidade de itens e itens não encontrados ao lado
-    doc.setTextColor(0, 0, 0); // Cor preta
     doc.setFontSize(16);
+    doc.setTextColor(0, 0, 0);
     doc.text(
       `Relatório de Produtos (${totalItems} itens, ${notFoundCount} não encontrados)`,
       20,
-      20,
+      marginTop
     );
-
-    // Posição inicial para o conteúdo
-    let yPosition = 30;
-
-    codes.forEach((code) => {
+  
+    // Iteração sobre os códigos
+    codes.forEach((code, index) => {
       const foundProduct = jsonData.find(
-        (item) => item.code.toLowerCase() === code.toLowerCase(),
+        (item) => item.code.toLowerCase() === code.toLowerCase()
       );
-
+  
       if (foundProduct) {
-        doc.setTextColor(0, 0, 0); // Cor preta para produto encontrado
+        doc.setTextColor(0, 0, 0);
         doc.setFontSize(12);
         doc.text(
-          `Cód: ${foundProduct.code} | Nome: ${foundProduct.title} | R$${foundProduct.price}`,
+          `Cód: ${foundProduct.code} | Nome: ${foundProduct.title} | R$:${foundProduct.price}`,
           20,
-          yPosition,
+          yPosition
         );
       } else {
-        doc.setTextColor(204, 0, 0); // Cor vermelha para produto não encontrado
+        doc.setTextColor(204, 0, 0);
         doc.setFontSize(12);
         doc.text(`Cód: ${code} - Produto não encontrado`, 20, yPosition);
       }
-      yPosition += 10; // Incrementa a posição para o próximo item
+  
+      yPosition += lineSpacing; // Move para a próxima linha
+  
+      // Verifica se ultrapassou a altura da página
+      if (yPosition > pageHeight - marginTop) {
+        doc.addPage(); // Adiciona uma nova página
+        yPosition = marginTop; // Reseta a posição do texto para o topo da nova página
+      }
     });
-
-    // Gerar o PDF
+  
+    // Salva o PDF
     doc.save('relatorio_produtos.pdf');
   }
+  
 
   return (
     <Container>
