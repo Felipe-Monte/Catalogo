f765e937babb9738b0cce19953fb93050338f63e
Felipe <carlosfelipemonte1997@gmail.com>
2025-02-04 12:01:34 -0300

    Feat: update list

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -2,14 +2,14 @@ import { Routes, Route } from 'react-router-dom';
 
 import { Home } from '../pages/Home';
 import Details from '../pages/Details';
-import Test from '../pages/List/main';
+import List from '../pages/List';
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/details/:code" element={<Details />} />
-      <Route path="/list" element={<Test />} />
+      <Route path="/list" element={<List />} />
     </Routes>
   );
 }
