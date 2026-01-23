e538517087aafaf634a5309648b5f1951e0504e4
Felipe <carlosfelipemonte1997@gmail.com>
2026-01-23 10:24:16 -0300

    fix: remove loading

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { FaCartPlus } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
 import { CardImg, CardText, Container } from './styles';
@@ -17,8 +17,6 @@ const Cards = ({
   type,
 }) => {
   const navigate = useNavigate();
-  const [imageLoaded, setImageLoaded] = useState(false);
-
   const { addToCart } = React.useContext(UserContext);
 
   function handleCardClick() {
@@ -31,39 +29,26 @@ const Cards = ({
 
   function handleCartClick(e) {
     e.stopPropagation();
-    const item = {
-      code,
-      title,
-      price,
-    };
-
-    addToCart(item);
+    addToCart({ code, title, price });
   }
 
   return (
     <Container className={`animeLeft ${isNew ? 'new_item' : ''}`}>
       {isNew && <p className="icon-new">NOVIDADE</p>}
+
       <CardImg
         onClick={handleCardClick}
         className={isAvailable ? '' : 'unavailable'}
       >
-        {!imageLoaded && (
-          <img
-            id="loader"
-            src="/spinner.svg"
-            alt="loading spinner"
-            loading="lazy"
-          />
-        )}
         <img
           src={imgUrl}
           alt={`Imagem de ${title}`}
           loading="lazy"
-          onLoad={() => setImageLoaded(true)}
-          onError={() => setImageLoaded(true)}
-          style={{ display: imageLoaded ? 'block' : 'none' }}
         />
-        {!isAvailable && <div className="unavailable-banner">Esgotado</div>}
+
+        {!isAvailable && (
+          <div className="unavailable-banner">Esgotado</div>
+        )}
       </CardImg>
 
       <CardText>
@@ -71,19 +56,15 @@ const Cards = ({
 
         <div className="container-category">
           <p>Cód: {code}</p>
-          {/* <p className="p-category">{category}</p> */}
           <p>{type}</p>
         </div>
 
         <div className="container-price">
-          {' '}
           <button className="btnAddCart" onClick={handleCartClick}>
             <FaCartPlus size={18} />
           </button>
           <span>{price}</span>
         </div>
-
-        {/* <p id="share">{share}</p> */}
       </CardText>
     </Container>
   );
